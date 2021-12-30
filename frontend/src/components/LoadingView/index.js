f3a1f2906eeda8d45a383c4698e1db75752d100a
thedevyansh <schawla333333@gmail.com>
2021-12-30 10:56:28 +0530

    Add Loading spinner

---
+++ frontend/src/components/LoadingView/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Center, Spinner } from '@chakra-ui/react';
+
+function LoadingView() {
+  return (
+    <Center h='65vh'>
+      <Spinner
+        thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='blue.300'
+        size='xl'
+      />
+    </Center>
+  );
+}
+
+export default LoadingView;
