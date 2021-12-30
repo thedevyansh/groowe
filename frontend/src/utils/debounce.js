f9cbac23480dfd3462f549a0717fa08022a5eb34
thedevyansh <schawla333333@gmail.com>
2021-12-30 16:55:22 +0530

    Add debounce function to improve performace of quering rooms

---
+++ frontend/src/utils/debounce.js
@@ -0,0 +1,20 @@
+export default function debounce(func, wait, immediate) {
+  let timeout;
+
+  return (...args) => {
+    const context = this;
+
+    const later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    const callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+}
