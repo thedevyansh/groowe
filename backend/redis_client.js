d5e80e0384d42078f32d42e258fbeae6d93d73ed
thedevyansh <schawla333333@gmail.com>
2022-01-06 12:44:17 +0530

    Add redis client and redis adapter

---
+++ backend/redis_client.js
@@ -0,0 +1,59 @@
+import redis, { createClient } from 'redis';
+import rejson from 'redis-rejson';
+import redisearch from 'redis-redisearch';
+import { promisify } from 'util';
+import config from './config';
+
+rejson(redis);
+redisearch(redis);
+
+redis.addCommand('ft.aggregate');
+redis.addCommand('lmove');
+
+const redisClient = createClient({
+  host: config.redisHost,
+  port: config.redisPort,
+});
+
+const ftcreateAsync = promisify(redisClient.ft_create).bind(redisClient);
+const keysAsync = promisify(redisClient.keys).bind(redisClient);
+const del = promisify(redisClient.del).bind(redisClient);
+
+export const roomsIndex = 'roomsIndex';
+
+redisClient.on('ready', async () => {
+  console.log(`Connected to Redis on ${config.redisHost}.`);
+  // Delete stale data
+  const roomKeys = await keysAsync('room:*');
+  const queueKeys = await keysAsync('queue:*');
+  const socketKeys = await keysAsync('socket:*');
+
+  if (roomKeys?.length) {
+    await del(roomKeys);
+  }
+
+  if (queueKeys?.length) {
+    await del(queueKeys);
+  }
+
+  if (socketKeys?.length) {
+    await del(socketKeys);
+  }
+
+  // Create secondary index on fields for efficient searching
+  await ftcreateAsync(roomsIndex,
+    'PREFIX', '1', 'room:',
+    'SCHEMA',
+    'name', 'TEXT', 'SORTABLE',
+    'description', 'TEXT', 'SORTABLE',
+    'genres', 'TAG', 'SORTABLE',
+    'numMembers', 'NUMERIC', 'SORTABLE',
+    'private', 'TAG', 'SORTABLE'
+  )
+});
+
+redisClient.on('error', err => {
+    console.error(err);
+  });  
+
+export default redisClient;
---
+++ backend/socketio_server.js
@@ -0,0 +1,12 @@
+import { Server } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import redisClient from './redis_client';
+
+const server = new Server();
+
+const pubClient = redisClient.duplicate();
+const subClient = redisClient.duplicate();
+
+server.adapter(createAdapter(pubClient, subClient));
+
+export default server;
