4b38ef5e7ac65faf46d57d206098ee20706e7eb7
thedevyansh <schawla333333@gmail.com>
2022-01-14 10:31:22 +0530

    change socket server

--- frontend/src/contexts/socket.js
+++ frontend/src/contexts/socket.js
@@ -1,7 +1,9 @@
 import { createContext } from 'react';
 import io from 'socket.io-client';
 const SOCKET_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'https://temporaldj.herokuapp.com';
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
