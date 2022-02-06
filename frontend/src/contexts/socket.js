45a7f9a7bd9027cabf9d968e0da91f2d47d086db
thedevyansh <schawla333333@gmail.com>
2022-02-06 11:51:27 +0530

    Fix backend url

--- frontend/src/contexts/socket.js
+++ frontend/src/contexts/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 const SOCKET_URL =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
-    : ['https://temporaldj.netlify.app', 'https://temporaldj.tech'];
+    : 'https://temporaldj.herokuapp.com';
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
--- frontend/src/services/song.js
+++ frontend/src/services/song.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL:
     !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
       ? 'http://localhost:5000'
-      : ['https://temporaldj.netlify.app', 'https://temporaldj.tech'],
+      : 'https://temporaldj.herokuapp.com',
   withCredentials: true,
 });
 
