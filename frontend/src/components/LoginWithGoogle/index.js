9f4525c1d9103024fe7881965d56ba547c3b3d20
thedevyansh <schawla333333@gmail.com>
2022-04-08 22:02:22 +0530

    Minor fix in URI

--- frontend/src/components/LoginWithGoogle/index.js
+++ frontend/src/components/LoginWithGoogle/index.js
@@ -5,7 +5,7 @@ import GoogleButton from 'react-google-button';
 const URI =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000/api/auth/google'
-    : 'https://temporaldj.herokuapp.com//api/auth/google';
+    : 'https://temporaldj.herokuapp.com/api/auth/google';
 
 function LoginWithGoogle() {
   return (
