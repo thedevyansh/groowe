b6d482a60b77bb533f0fb30d2f91a02e9f585da4
thedevyansh <schawla333333@gmail.com>
2022-04-30 16:30:19 +0530

    Improve Google login btn

--- frontend/src/components/LoginWithGoogle/index.js
+++ frontend/src/components/LoginWithGoogle/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import GoogleButton from 'react-google-button';
+import './buttonStyle.css';
 
 const URI =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
@@ -10,11 +10,20 @@ const URI =
 function LoginWithGoogle() {
   return (
     <Box align='center' mt='30px'>
-      <GoogleButton
+      <div
+        class='google-btn'
         onClick={() => {
           window.open(URI, '_self');
-        }}
-      />
+        }}>
+        <div class='google-icon-wrapper'>
+          <img
+            class='google-icon'
+            src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+            alt="Google logo"
+          />
+        </div>
+        <p class='btn-text'>Sign in with Google</p>
+      </div>
     </Box>
   );
 }
